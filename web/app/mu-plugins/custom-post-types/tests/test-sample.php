77d567c9ee501f94aa1b06a65032a1f06d593766
nomad-mystic <nomadmystics@gmail.com>
2018-10-17 16:05:03 -0700

    master: Added tests suite for plugin development.

---
+++ web/app/mu-plugins/custom-post-types/tests/test-sample.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\MuPlugins\CustomPostType;
+
+/**
+ * Class SampleTest
+ *
+ * @package Custom_Post_Types
+ */
+
+//use WP_UnitTestCase;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends TestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
