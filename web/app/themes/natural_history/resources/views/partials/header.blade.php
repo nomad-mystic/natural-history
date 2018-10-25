e380f2aea3dcf032504695451e2eb736475f8a0c
nomad-mystic <nomadmystics@gmail.com>
2018-10-25 13:48:49 -0700

    master: Worked throught geting some of the fonts setup by researching what the museum was using and fixed myself up a logo for the menu.

--- web/app/themes/natural_history/resources/views/partials/header.blade.php
+++ web/app/themes/natural_history/resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
-<header class="banner">
-  <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
+<header class="">
+  <div class="primary-menu">
+    <a class="" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+    <nav class="">
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
       @endif
